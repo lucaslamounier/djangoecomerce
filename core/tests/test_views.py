329e24431e156c8995e6e4432bbb406331889033
Lucas Lamounier Sirqueira <lucas@localhost.localdomain>
2017-01-06 16:43:14 -0200

    add core application and views and tests

---
+++ core/tests/test_views.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+class  IndextViewTestCase(TestCase):
+
+    def setUp(self):
+        ''' Executado quando inicia cada teste '''
+        self.client = Client()
+        self.url = reverse('index')
+
+    def tearDown(self):
+        ''' Executado quando acaba cada teste '''
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html')
--- core/views.py
+++ core/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	texts = ['lorem ipsum', 'consectetur adipisicing elit',
-			'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.']
-	context = {
-		'title': 'Django e-commerce',
-		'texts': texts,
-	}
-	return render(request, 'index.html', context)
+	return render(request, 'index.html')
+
+def contact(request):
+	return render(request, 'contact.html')
+
+def product_list(request):
+	return render(request, 'product_list.html')
+
+def product(request):
+	return render(request, 'product.html')
