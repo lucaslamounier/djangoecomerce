88b50f35d0ff98a264478b4c08829bd19395dcd9
Lucas Lamounier Sirqueira <lucas@localhost.localdomain>
2017-01-12 15:13:11 -0200

    add new configuration for database

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = 'djangoecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 DATABASES = {
-    'default': {}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 #DATABASES = {
